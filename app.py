f5d290cd47f636285611a6699ae148124ca61294
ghalibbajwa <ghalibs911@gmail.com>
2024-05-16 00:51:03 +0500

    LENGHT

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ def predict(
                 None,
                 None,
             )
-        if len(prompt) > 200:
+        if len(prompt) < 2:
             gr.Warning(
                 "Text length limited to 200 characters for this demo, please try shorter text. You can clone this space and edit code for your own usage"
             )
